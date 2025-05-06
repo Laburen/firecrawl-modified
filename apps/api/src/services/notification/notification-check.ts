7537268df40cbfa538850c17f290f44de827b118
Germiquere <german150703.miquere@gmail.com>
2025-05-06 19:18:09 -0300

    returning false when entering in the notification-check

--- apps/api/src/services/notification/notification-check.ts
+++ apps/api/src/services/notification/notification-check.ts
@@ -3,6 +3,7 @@ import { isEnterpriseTeamCreatedAfterRateLimitChange } from "../subscription/ent
 export async function shouldSendConcurrencyLimitNotification(
   team_id: string,
 ): Promise<boolean> {
+  return false
   const isEnterprise =
     await isEnterpriseTeamCreatedAfterRateLimitChange(team_id);
   return !isEnterprise;
